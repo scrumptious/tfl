735ab5707de945fc722d3997183957d028d9b395
scrumptious <ljasiurski@gmail.com>
2020-02-21 11:21:32 +0000

    styling changes

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -15,12 +15,38 @@
         width: 60vw;
         margin: 4vh auto;
         padding: 0 20px 30px;
+        border: 2px solid black;
+        padding: 0 20px 30px;
+        background-color: lightgrey;
     }
     table {
         width: 100%;
+        border: 2px solid black;
+        border-collapse: collapse;
+
+
     }
     table th {
         width: 30%;
+        text-align: left;
+        background-color: lightgrey;
+
+    }
+    table td {
+        padding: 4px;
+    }
+    table th, td {
+        border-right: 2px solid black;
+    }
+    table tr:nth-child(odd) {
+        background-color: white;
+    }
+    table tr:nth-child(even) {
+        background-color: #f3f3f3;
+    }
+    td a {
+        display: block;
+        text-align: center;
     }
 </style>
 <h1 class="lead">Travel Widget</h1>
